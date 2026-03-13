067f92b098d4a6ea222473d85319b42504580253
karbon <giwilli@ethz.ch>
2026-03-13 14:29:06 +0100

    Added LQR Reference

--- MPC_Delft.m
+++ MPC_Delft.m
@@ -3,6 +3,9 @@ B = B_lin_s;
 C = C_lin_s;
 D = D_lin_s;
 
+
+
+
 Ts = 0.01;
 sys_d = c2d(ss(A,B, C, D),Ts,'tustin');
 
@@ -20,7 +23,9 @@ dim_B = size(B,2);
 dim_C = size(C,1);
 Q = 1000*eye(dim_A);
 R = 1*eye(dim_B);
+x0 = [-0.1;-0.1;-0.1;-0.1];
 
+%%
 c = [Inf; 7; pi/18; Inf];
 u_bound = 42;
 
@@ -38,8 +43,6 @@ model.u.penalty = QuadFunction(R);
 Tset = model.LQRSet;
 P = model.LQRPenalty.weight;
 
-
-x0 = [-0.1;-0.1;-0.1;-0.1];
 
 T = zeros(dim_A*(N+1),dim_A);
 S = zeros(dim_A*(N+1), dim_B*N);
@@ -103,6 +106,8 @@ g = b_bar - D_bar*T_tilde*x0;
 M = 1000;
 x = zeros(dim_A, (M+1));
 x(:,1) = x0;
+u_log = zeros(dim_B, M+1);
+u_log(:,1) = 0;
 
 for i = 1:M
     disp(i);
@@ -110,9 +115,28 @@ for i = 1:M
     h = S.'*Q_bar*T*x0;
     g = b_bar - D_bar*T_tilde*x0;
     u = quadprog(H,h, G,g);
+    u_log(:,i) = u;
     x(:,i+1) = sys_d.A*x(:,i) + sys_d.B*u(1);
 end
-%%
+%% LQR for reference
+[K,S,P] = lqr(sys_d,Q,R);
+M = 1000;
+x_lqr = zeros(dim_A, (M+1));
+x_lqr(:,1) = x0;
+u_log_lqr = zeros(dim_B, M+1);
+u_log_lqr(:,1) = 0;
 
+for i = 1:M
+    %disp(i);
+    u = -K*x(:,i);
+    x_lqr(:,i+1) = (sys_d.A*x_lqr(:,i) + sys_d.B*u);
+    u_log_lqr(:,i) = u;
+end
+%%
+hold on
 t = 0:0.01:M*0.01;
 plot(t,x)
+plot(t, u_log)
+
+plot(t,x_lqr)
+plot(t,u_log_lqr)
